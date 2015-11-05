ff9ae57c7cb39215503a5eda1120965194e78b61
Michael Nesvold <mdnesvold@gmail.com>
2015-11-05 16:10:03 -0500

    +support for multiple test files

---
+++ tests/bare.c
@@ -0,0 +1,4 @@
+void
+ping ( void )
+{
+}
---
+++ tests/bare.expected.js
@@ -0,0 +1,12 @@
+(function (globals) {
+    var ASM = function (stdlib, ffi, heap) {
+        'use asm';
+        function _ping() {
+            return;
+        }
+        return { _ping: _ping };
+    };
+    var asm = ASM(globals);
+    var adaptors = { ping: asm._ping };
+    globals.ASM = adaptors;
+}(this));
